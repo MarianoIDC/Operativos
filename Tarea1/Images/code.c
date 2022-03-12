303df3e2935baad499ac2c2c75d70bcd224a1f17
geraldvm <gerald-_16@hotmail.com>
2022-03-12 14:25:11 -0600

    Get size of Image

--- Tarea1/Images/code.c
+++ Tarea1/Images/code.c
@@ -8,15 +8,34 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 
-
+void load_file(char *imageName);
+int size_of_image(char *imageName);
 int main(void) {
+    char *imageName="sunset_940_530.jpg";
+    load_file(imageName);
+    int size= size_of_image(imageName);
+}
+//gcc -std=c17 -Wall code.c -o code -lm
+//./code
+//char *s  = "geeksquiz";
+void load_file(char *imageName){
     int width, height, channels;
-    unsigned char *img = stbi_load("sunset_940_530.jpg", &width, &height, &channels, 0);
+    unsigned char *img = stbi_load(imageName, &width, &height, &channels, 0);
     if(img == NULL) {
         printf("Error in loading the image\n");
         exit(1);
     }
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 }
-//gcc -std=c17 -Wall code.c -o code -lm
-//./code
+
+int size_of_image(char *imageName){
+    int width, height, channels;
+    unsigned char *img = stbi_load(imageName, &width, &height, &channels, 0);
+    if(img == NULL) {
+        printf("Error in loading the image\n");
+        exit(1);
+    }
+    int size= width*height;
+    printf("The size of image is: %d px\n", size);
+    return size;
+}
