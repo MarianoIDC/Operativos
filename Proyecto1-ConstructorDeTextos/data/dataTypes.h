6dad28111b640017c38afe2f6517cfb88b446487
Aykull <luciamongeg@gmail.com>
2022-04-15 14:21:26 -0600

    Structs to access shared memory (approach changed)

---
+++ Proyecto1-ConstructorDeTextos/data/dataTypes.h
@@ -0,0 +1,56 @@
+#ifndef DATATYPE
+#define DATATYPE
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <semaphore.h>
+#include <time.h>
+
+typedef struct{
+    int key;
+    int index;
+    time_t current_time;
+    int data;
+
+}data;
+
+//global data, should be share with all process
+typedef struct{
+    semaph semaphores;
+    int size;
+    buffer buff;
+    bool stop;
+    int N;
+    char data[];
+
+}init_global;
+
+//buffer data 
+typedef struct{
+    int head;
+    int tail;
+    int mxlen;
+
+}buffer;
+
+//stats info
+typedef struct{
+    int data;
+    double block_client;
+    double block_recons;
+    int transf;
+    double total_memory;
+    double total_time;
+}stats;
+
+//semaphores needed
+typedef struct{
+    sem_t usage_sem;
+    sem_t full;
+    sem_t empty;
+}semaph;
+
+
+
+
+#endif
