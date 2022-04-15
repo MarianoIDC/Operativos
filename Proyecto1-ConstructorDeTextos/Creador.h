2ebaeea3454716d4f77c5b685c603fdb00a38106
Aykull <luciamongeg@gmail.com>
2022-04-15 02:19:44 -0600

    Try memory share

---
+++ Proyecto1-ConstructorDeTextos/Creador.h
@@ -0,0 +1,13 @@
+#ifndef CREATHOR
+#define CREATHOR
+
+extern int creator();
+
+extern int* globBlock; //global 
+extern int* memBlock; //memory block
+extern int* dtBlock; //date and time block
+extern int* statsBlock; //stats block
+extern int size; 
+extern int id;
+
+#endif
---
+++ Proyecto1-ConstructorDeTextos/Test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include "Creador.h"
+
+
+
+int main(){
+    //creator();
+    
+    printf("ID desde Test: %ls\n", memBlock);
+}
