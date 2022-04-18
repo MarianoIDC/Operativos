1bdc8dcb47b098c6a491b4f32d3ac883db3621f9
Mariano <mmunoz@estudiantec.cr>
2022-04-17 22:47:05 -0600

    Borrar dato

--- Proyecto1-ConstructorDeTextos/Cliente/readFile.c
+++ Proyecto1-ConstructorDeTextos/Cliente/readFile.c
@@ -9,6 +9,8 @@
 int main()
 {
 	FILE* ptr;
+	FILE* ptr_s;
+	FILE* ptr_c;
 	char ch;
     time_t t;
     struct tm *tm;
@@ -16,31 +18,69 @@ int main()
 
 	// Opening file in reading mode
 	ptr = fopen("test.txt", "r");
-
+	ptr_s = fopen("test.txt", "r");
+	ptr_c = fopen("test.txt", "r");
 	if (NULL == ptr) {
 		printf("file can't be opened \n");
 	}
 
 	printf("Press Enter to read the file... \n");
 
+	
+	char ch_size;
+	ch_size = fgetc(ptr_s);
+	int size = 0;
+
+	//largo del archivo
+	while (ch_size != EOF)
+    {
+        ch_size = fgetc(ptr_s);
+		size++;
+    }
+
+
+	//Creacion del char
+	char ch_aux;
+	char file[size+1];
+	ch_aux = fgetc(ptr_c);
+	int n = 0;
+
+	for (int i = 0; i <= size; i++)
+	{
+		if (ch_aux == EOF)
+		{
+			break;
+		}
+		else{
+			file[i] = ch_aux;
+			ch_aux = fgetc(ptr_c);
+		}
+	}
+    
 	// Printing what is written in file
 	// character by character using loop.
-	do {
+	int j = 1;
+	while (ch != EOF) {
         char c = getchar();
         if (c==10)
         {
+			printf("|--------------------------------------------------------|\n");
+			if(j < size)
+			{
+				printf("%s\n", &file[j]);
+			}
+			j++;
             ch = fgetc(ptr);
             t=time(NULL);
             tm=localtime(&t);
             strftime(fechayhora, 100, "%d/%m/%Y-%H:%M:%S", tm);
             printf ("Dato: %c, logtime>>> %s\n", ch, fechayhora);
-
-            
+			printf("|--------------------------------------------------------|\n"); 
         }
         
 		// Checking if character is not EOF.
 		// If it is EOF stop eading.
-	} while (ch != EOF);
+	} 
 
 	// Closing the file
 	fclose(ptr);
